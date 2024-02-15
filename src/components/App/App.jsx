27b69c590ff97970582ffaec59a31df89c864fc9
Josh Rudesill <joshrudesill@gmail.com>
2024-02-15 09:59:36 -0600

    Basic React Scaffolding

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,18 +1,21 @@
-import React from 'react';
+import React from "react";
 
-import Header from '../Header/Header.jsx'
-import './App.css';
-
+import Header from "../Header/Header.jsx";
+import "./App.css";
+import GroceryForm from "../GroceryForm/GroceryForm.jsx";
+import ShoppingList from "../ShoppingList/ShoppingList.jsx";
 
 function App() {
-    return (
-        <div className="App">
-            <Header />
-            <main>
-                <p>Under Construction...</p>
-            </main>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <Header />
+      <main>
+        <p>Under Construction...</p>
+        <GroceryForm />
+        <ShoppingList />
+      </main>
+    </div>
+  );
 }
 
 export default App;
