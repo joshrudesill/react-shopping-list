ce7d94b86aa9bea52751f6233a8982247ae009f5
Ryan Kamleiter <kamleiterryan@gmail.com>
2024-02-15 10:38:38 -0600

    WIP

--- database.sql
+++ database.sql
@@ -1,2 +1,12 @@
 -- Don't forget to add your create table SQL 
 -- It is also helpful to include some test data
+
+CREATE TABLE groceries (
+    id SERIAL PRIMARY KEY,
+    item_name VARCHAR(80) NOT NULL,
+    quantity DECIMAL NOT NULL,
+    unit VARCHAR(20),
+    purchased BOOLEAN DEFAULT FALSE
+);
+
+SELECT * FROM groceries;
